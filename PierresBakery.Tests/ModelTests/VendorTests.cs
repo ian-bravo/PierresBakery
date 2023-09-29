8098766d5d8a2ebb291e97731abda0329825df04
ian-bravo <bravo.ian@gmail.com>
2023-09-29 14:26:49 -0700

    pass vendor GetName test

--- PierresBakery.Tests/ModelTests/VendorTests.cs
+++ PierresBakery.Tests/ModelTests/VendorTests.cs
@@ -10,14 +10,32 @@ namespace PierresBakery.Tests
   {
     public void Dispose()
     {
-      // Code inside this method is run between each test.
+      Vendor.ClearAll();
     }
 
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("test vendor-name");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "test vendor-name";
+      Vendor newVendor = new Vendor(name);
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      string name = "test vendor-name";
+      Vendor newVendor = new Vendor(name);
+      int result = newVendor.Id;
+      Assert.AreEqual(1, result);
     }
   }
 }
--- PierresBakery/Models/Vendor.cs
+++ PierresBakery/Models/Vendor.cs
@@ -4,10 +4,20 @@ namespace PierresBakery.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public int Id { get; }
 
-    public Vendor()
+    public Vendor(string vendorName)
     {
-    
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
