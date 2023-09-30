1174810ef0cea57c4ed50fc3170fdd174e824132
ian-bravo <bravo.ian@gmail.com>
2023-09-29 22:38:33 -0700

    route vendors and orders

--- PierresBakery/Controllers/VendorsController.cs
+++ PierresBakery/Controllers/VendorsController.cs
@@ -21,11 +21,22 @@ namespace PierresBakery.Controllers
       return View();
     }
 
-    [HttpPost('/vendors')]
+    [HttpPost("/vendors")]
     public ActionResult Create(string vendorName, string vendorDescription)
     {
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
-      return RedirectToAction("Index", newVendor);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object> ();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model); 
     }
 
   }
